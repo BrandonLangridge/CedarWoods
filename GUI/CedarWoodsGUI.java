61e10bc0565707d309695fd2499824a1a0254e2b
GrahamUWE <graham2.walker@live.uwe.ac.uk>
2026-03-11 14:22:33 +0000

    Add files via upload

--- GUI/CedarWoodsGUI.java
+++ GUI/CedarWoodsGUI.java
@@ -5,26 +5,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.io.InputStream;
 
-public class CedarWoodsGUI extends Application
-{
+public class CedarWoodsGUI extends Application {
+
     @Override
     public void start(Stage primaryStage)
-            throws IOException
-    {
+            throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("CedarWoodsGUIFXML.fxml"));
-        
+
         primaryStage.setTitle("Cedar Woods System");
         Scene helloworldScene = new Scene(loader.load());
-        
+
         primaryStage.setScene(helloworldScene);
+
+        InputStream iconStream = getClass().getResourceAsStream("/gui/app_icon.png");
+        if (iconStream != null) {
+            primaryStage.getIcons().add(new Image(iconStream));
+        } else {
+            System.err.println("Icon not found at /gui/app_icon.png");
+        }
+
         primaryStage.show();
     }
-    
-   public static void main(String[] args)
-   {
-       launch(args);
-   }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-    
-  
